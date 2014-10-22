6b380949b300f74c8c6d3b7ae57d224b30f78c43
mantljir <george.mantlik@gmail.com>
2014-10-22 22:25:27 +0200

    song listing in songbook

--- app/api/frontend/version1/SongbooksResource.php
+++ app/api/frontend/version1/SongbooksResource.php
@@ -3,11 +3,13 @@
 namespace FrontendApi\Version1;
 
 
+use App\Model\Entity\Song;
 use App\Model\Entity\Songbook;
 use App\Model\Service\SessionService;
 use FrontendApi\FrontendResource;
 use Kdyby\Doctrine\EntityManager;
 use Markatom\RestApp\Api\Response;
+use Markatom\RestApp\Routing\AuthorizationException;
 
 /**
  * @todo	Fill desc.
@@ -24,7 +26,51 @@ class SongbooksResource extends FrontendResource {
         $this->em = $em;
     }
 
+
     /**
+     * Reads detailed information about songbook.
+     * @param int $id
+     * @return Response
+     */
+    public function read($id)
+    {
+        $songbook = $this->em->getDao(Songbook::class)->find($id);
+
+        if (!$songbook) {
+            return Response::json([
+                'error' => 'UNKNOWN_SONGBOOK',
+                'message' => 'Songbook with given id not found.'
+            ])->setHttpStatus(Response::HTTP_NOT_FOUND);
+        }
+
+        if(!$songbook->public){
+            $this->assumeLoggedIn();
+
+            if($this->getActiveSession()->user !== $songbook->owner){
+                throw new AuthorizationException;
+            }
+        }
+
+        $songs = array_map(function (Song $song){
+            return [
+                'id'              => $song->id,
+                'title'           => $song->title,
+                'album'           => $song->album,
+                'author'          => $song->author,
+                'originalAuthor'  => $song->originalAuthor,
+                'year'            => $song->year
+            ];
+        }, $songbook->songs);
+
+        return Response::json([
+            'id'             => $songbook->id,
+            'name'          => $songbook->name,
+            'songs'      => $songs
+        ]);
+    }
+
+    /**
+     * Returns brief information about all user's songbooks.
      * @return Response
      */
     public function readAll()
